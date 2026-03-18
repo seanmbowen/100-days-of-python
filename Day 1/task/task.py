e418893989b50012359c160ae6a48524f16e6744
Sean M. Bowen <sbowen@gmail.com>
2026-03-17 23:15:24 -0400

    Mark Day 1 tasks as solved and update solutions for "Variable Naming" and "Band Name Generator" tasks.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,5 @@
-print("Hello " + input("What is your name? ") + "!")
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city} {pet}")
